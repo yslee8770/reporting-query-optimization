79a28dcd93763cfa5043a1c376b0912b6d48adfa
yslee <mocuri8770@gmail.com>
2025-12-25 10:29:46 +0900

    first commit

---
+++ src/main/java/com/example/reporting_query_optimization/ReportingQueryOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.reporting_query_optimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingQueryOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingQueryOptimizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reporting_query_optimization/ReportingQueryOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reporting_query_optimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingQueryOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
